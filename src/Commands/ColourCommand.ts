1acdd14276c2f9ef198301a883706f391203acf5
Matthew Davis <matthew@matthewdavis.io>
2019-05-23 10:48:03 -0500

    first commit

---
+++ src/Commands/ColourCommand.ts
@@ -0,0 +1,48 @@
+import { Command, CommandBase, CommandParser, Event } from '@autobot/common';
+import { RichEmbed }                                  from "discord.js";
+
+/**
+ *
+ */
+@Command
+export class ColourCommand extends CommandBase {
+
+    public constructor() {
+
+        //
+        // Set this commands configuration.
+        //
+        super({
+
+            event: Event.MESSAGE,
+            name: 'colour',
+            group: 'fun',
+            description: 'Returns an embed with the color passed to it.'
+
+        });
+
+    }
+
+    /**
+     * Called when a command matches config.name.
+     *
+     * @param command Parsed out commamd
+     *
+     */
+    public async run(command: CommandParser) {
+
+
+        const matches = command.arguments[ 0 ].name.match(/>color #(.*)/);
+
+        if (matches.length > 0) {
+
+            const embed = new RichEmbed().setColor(parseInt(matches[ 1 ], 16))
+                                         .setDescription(`#${ matches[ 1 ] }`);
+
+            command.obj.channel.send(embed);
+
+        }
+
+    }
+
+}
